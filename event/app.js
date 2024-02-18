1f0ba2931e12b1e25c221b6e6966168c189edb03
pasukanbocil <ctian8787@gmail.com>
2024-02-18 17:10:15 +0700

    docs: :memo: plis mel

--- event/app.js
+++ event/app.js
@@ -3,3 +3,24 @@ const button = document.querySelector('#clickme');
 button.onclick = function(){
     console.log('You clicked me!');
 };
+
+const scream    = () => {
+    console.log('Ahhhhhhhhhh!');
+};
+
+button.onmouseover = scream;
+
+const eventbtn = document.querySelector('#eventbtn');
+
+eventbtn.addEventListener('click', step1);
+eventbtn.addEventListener('click', step2);
+
+function step1(){
+    console.log('Step 1');
+}
+
+function step2(){
+    console.log('Step 2');
+}
+
+// eventbtn.onclick = step1;

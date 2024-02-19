90ba9ee20a285eeb18b6e14461c722abcef31c1a
pasukanbocil <ctian8787@gmail.com>
2024-02-19 15:53:23 +0700

    feat: :ambulance: Prabowo Gemoy Meli Lebih Gemoy

--- event/app.js
+++ event/app.js
@@ -1,26 +1,11 @@
-const button = document.querySelector('#clickme');
+const form = document.querySelector('#form');
 
-button.onclick = function(){
-    console.log('You clicked me!');
-};
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Form submitted');
+});
 
-const scream    = () => {
-    console.log('Ahhhhhhhhhh!');
-};
-
-button.onmouseover = scream;
-
-const eventbtn = document.querySelector('#eventbtn');
-
-eventbtn.addEventListener('click', step1);
-eventbtn.addEventListener('click', step2);
-
-function step1(){
-    console.log('Step 1');
-}
-
-function step2(){
-    console.log('Step 2');
-}
-
-// eventbtn.onclick = step1;
+document.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Link clicked');
+});

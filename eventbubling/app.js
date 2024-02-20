cc380173a91ee37b5c48b177e961afc1a7bb2b36
pasukanbocil <ctian8787@gmail.com>
2024-02-20 14:19:58 +0700

    docs: :memo: 27 16 00 01

---
+++ eventbubling/app.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('#changeColor');
+const container = document.querySelector('#container');
+
+button.addEventListener('click', function() {
+    container.style.backgroundColor = generateRandomColor();
+});
+
+const generateRandomColor = () => {
+    
+    const r = Math.floor(Math.random() * 255 );
+    const g = Math.floor(Math.random() * 255 );
+    const b = Math.floor(Math.random() * 255 );
+
+    return `rgb(${r}, ${g}, ${b})`;
+};
--- eventprevent/app.js
+++ eventprevent/app.js
@@ -7,10 +7,10 @@ const list = document.querySelector('#notes');
 //     console.log('nilai nerubah');
 // });
 
-input.addEventListener('input', (e) => {
-    document.querySelector('h1').innerText = input.value;
-    console.log('input event');
-});
+// input.addEventListener('input', (e) => {
+//     document.querySelector('h1').innerText = input.value;
+//     console.log('input event');
+// });
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -20,4 +20,15 @@ form.addEventListener('submit', (e) => {
     list.append(newList);
     input.value = '';
 });
+
+list.addEventListener('click', (e) => {
+    e.target.nodeName === 'LI' && e.target.remove();
+});
+
+// const lis = document.querySelectorAll('li');
+// for (let li of lis){
+//     li.addEventListener('click',function(){
+//         li.remove();
+//     });
+// }
 
